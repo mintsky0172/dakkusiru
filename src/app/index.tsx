03de487e337583afe0eaf2eda4d7f0833b143eaa
mintsky0172 <mintsky0172@naver.com>
2026-04-15 19:13:53 +0900

    [chore] supabase 세팅

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,21 @@ import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
+import { supabase } from "../services/supabase/client";
 
 const HomeScreen = () => {
+
+  const test = async () => {
+    const {data, error} = await supabase
+        .from('sticker_packs')
+        .select('*');
+
+    console.log('data: ', data);
+    console.log('error: ', error);
+  }
+
+  test();
+
   return (
     <SafeAreaView style={{ flex: 1, padding: 24, justifyContent: "center" }}>
       <Text style={{ fontSize: 28, fontWeight: "700", marginBottom: 24 }}>
---
+++ src/services/supabase/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL?.trim();
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY?.trim();
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase environment variables are missing.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
